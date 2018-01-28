55b10355cbf411655593e2705a00fba1add2cafe
MrPowers <matthewkevinpowers@gmail.com>
2018-01-28 17:58:52 -0500

    Update the spark-daria documentation

--- scripts/update_spark_daria_documentation.sh
+++ scripts/update_spark_daria_documentation.sh
@@ -1,4 +1,4 @@
-cp -rf $SPARK_DARIA_PATH/target/scala-2.11/api/ docs/spark_daria
-git commit -am "Update the spark_daria docs"
-git push origin master
+cp -r $SPARK_DARIA_PATH/target/scala-2.11/api/ docs/spark_daria
+# git commit -am "Update the spark_daria docs"
+# git push origin master
 
