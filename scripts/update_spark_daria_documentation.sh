ac4a35f990d87d388d8ceb1094e3853ff8761e20
MrPowers <matthewkevinpowers@gmail.com>
2018-03-15 16:26:47 -0500

    Add the spark-daria v0.18.0 documentation

--- scripts/update_spark_daria_documentation.sh
+++ scripts/update_spark_daria_documentation.sh
@@ -1,4 +1,13 @@
-cp -r $SPARK_DARIA_PATH/target/scala-2.11/api/ docs/spark_daria
+SPARK_DARIA_VERSION=$1
+if [ "$SPARK_DARIA_VERSION" = "" ]
+  then
+    echo "SPARK_DARIA_VERSION variable must be set"
+    exit 1
+fi
+
+mkdir docs/spark_daria/$SPARK_DARIA_VERSION
+cp -r $SPARK_DARIA_PATH/target/scala-2.11/api/ docs/spark_daria/$SPARK_DARIA_VERSION
+cp -r $SPARK_DARIA_PATH/target/scala-2.11/api/ docs/spark_daria/latest
 # git commit -am "Update the spark_daria docs"
 # git push origin master
 
